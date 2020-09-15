8b311ab3d972591516b8f0dbdc826eca6daa33e9
Shinya Inomata <shinya.inomata@gmail.com>
2020-09-15 16:12:55 +0900

    Initial Commit

---
+++ Memopad200914/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Memopad200914
+//
+//  Created by Shinya  on 2020/09/14.
+//  Copyright © 2020 Shinya . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //for Github upload...
+
+    
+}
+
